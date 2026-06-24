9ebb56a8860bc9156d2452a67d03d049753d6be8
OfflineBot <flintstonelg@gmail.com>
2026-06-24 20:53:17 +0200

    activate.sh: Service verlinken, damit Timer ihn triggern kann

--- activate.sh
+++ activate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -e
 
+# Service auffindbar machen (wird vom Timer getriggert, nicht selbst "enabled")
+systemctl --user link ~/.local/share/focus/focus_updater.service
+
+# Timer aktivieren + sofort starten (legt den Timer-Symlink selbst an)
 systemctl --user enable --now ~/.local/share/focus/focus_updater.timer
 
 systemctl --user daemon-reload
